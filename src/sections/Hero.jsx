1bac42bbb62dfc6748a40cdeb90106ee580732ad
[kiereyes0812] <[reyeskierbrian]>
2026-05-16 18:32:45 +0800

    Updated hero 3d image

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,89 @@
+import { words } from "../constants/index.js";
+import Button from "../components/Button.jsx";
+import HeroExperience from "../components/HeroModels/HeroExperience.jsx";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import AnimatedCounter from "../components/AnimatedCounter.jsx";
+
+const Hero = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".hero-text h1",
+      {
+        y: 50,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power2.inOut",
+        stagger: 0.2,
+      },
+    );
+  });
+  return (
+    <section id="hero" className="relative overflow-hidden">
+      <div className="absolute top-0 left-0 z-10">
+        <img src="/images/bg.png" alt="background" />
+      </div>
+
+      <div className="hero-layout">
+        {/* LEFT: HERO CONTENT */}
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <h1>
+                Shaping
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word) => (
+                      <span
+                        key={word.text}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className="xl:size-12 md:size-10 size-7 nd:p-2 p-1 rounded-full bg-white-50"
+                        />
+
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
+
+              <h1>into Real Projects</h1>
+              <h1>that Deliver Result</h1>
+            </div>
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+              Hi, I'm Kier, a passionate software developer with a knack for
+              transforming ideas, concepts, designs, and code into real projects
+              that deliver results. With a strong background in software
+              development and a keen eye for detail, I specialize in crafting
+              innovative solutions that meet the unique needs of clients across
+              various industries.
+            </p>
+            <Button
+              className="md:w-80 md:h-16 w-60 h-12"
+              id="button"
+              text="See my Work"
+            />
+          </div>
+        </header>
+
+        {/* RIGHT: 3D MODEL */}
+        <figure>
+          <div className="hero-3d-layout">
+            <HeroExperience />
+          </div>
+        </figure>
+      </div>
+      <AnimatedCounter />
+    </section>
+  );
+};
+
+export default Hero;
