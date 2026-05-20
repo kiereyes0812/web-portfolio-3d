c405adcaff3346e1067a7e8ba0f9d63a4ef0ca08
[kiereyes0812] <[reyeskierbrian]>
2026-05-21 05:31:02 +0800

    Added Tech Stach Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import Hero from "./sections/Hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 import NavBar from "./components/NavBar.jsx";
-import LogoSection from "./components/LogoSection.jsx";
+import LogoSection from "./sections/LogoSection.jsx";
+import FeatureCards from "./sections/FeatureCards.jsx";
+import ExperienceSection from "./sections/ExperienceSection.jsx";
+import TechStack from "./sections/TechStack.jsx";
 
 const App = () => {
   return (
@@ -10,8 +13,11 @@ const App = () => {
       <Hero />
       <ShowcaseSection />
       <LogoSection />
+      <FeatureCards />
+      <ExperienceSection />
+      <TechStack />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
